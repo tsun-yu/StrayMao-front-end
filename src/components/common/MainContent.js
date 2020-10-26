12cd715775d2ed900356d6f1fa86640067a45302
joart0725 <joart0725@gmail.com>
2020-10-27 00:44:30 +0800

    共同: 暫時定義有用到的標題樣式

--- src/components/common/MainContent.js
+++ src/components/common/MainContent.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from "react";
+import "../../styles/custom.scss";
 
 function MainContent(props) {
   return (
     <>
       <main className="flex-shrink-0">
         {/* props.children代表嵌入在其中的元件或資料內容 */}
-        <div className="container">{props.children}</div>
+        <div className="container">
+          {props.children}
+          <div className="Header1">標題一請自取</div>
+          <div className="Header2">標題二請自取</div>
+          <div className="Header3">標題三請自取</div>
+          <div className="Header4">標題四請自取</div>
+          <div className="Header5">標題五請自取</div>
+          <div className="Header6">標題六請自取</div>
+          <div className="Header7">標題七請自取</div>
+          <div className="Paragraph1">內文一請自取</div>
+          <div className="Paragraph2">內文二請自取</div>
+          <div className="caption">說明文字請自取</div>
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-export default MainContent
+export default MainContent;
