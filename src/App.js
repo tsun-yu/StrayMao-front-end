79d7a4731f3f07f14dff6faac14a43b48ca8037e
krok1029 <krok1029@gmail.com>
2020-10-27 18:12:53 +0800

    test page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import MyFooter from "./components/common/MyFooter";
 import MainContent from "./components/common/MainContent";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import PageBar from "./components/adoption/PageBar";
-import Page1 from './components/homapage/Page1';
+import Page1 from "./components/homapage/Page1";
+import TestLink from "./components/common/TestLink";
 
+import AdoptionTest from "./components/adoption/Test";
+import CartTest from "./components/cart/Test";
+import HomeTest from "./components/homapage/Test";
+import MemberTest from "./components/membership/Test";
+import SocialTest from "./components/social_media/Test";
+import StoreTest from "./components/store/Test";
 function App() {
   return (
     <Router>
@@ -18,7 +25,32 @@ function App() {
             <Route exact path="/">
               {/*要連線的網頁*/}
               {/* <Page1/> */}
-              <PageBar />
+              {/* <PageBar /> */}
+              <TestLink />
+            </Route>
+            <Route path="/adoptionTest">
+              {/*要連線的網頁*/}
+              <AdoptionTest />
+            </Route>
+            <Route path="/cartTest">
+              {/*要連線的網頁*/}
+              <CartTest />
+            </Route>
+            <Route path="/homeTest">
+              {/*要連線的網頁*/}
+              <HomeTest />
+            </Route>
+            <Route path="/memberTest">
+              {/*要連線的網頁*/}
+              <MemberTest />
+            </Route>
+            <Route path="/socialTest">
+              {/*要連線的網頁*/}
+              <SocialTest />
+            </Route>
+            <Route path="/storeTest">
+              {/*要連線的網頁*/}
+              <StoreTest />
             </Route>
           </Switch>
         </MainContent>
---
+++ src/components/cart/Test.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+
+function Test(props) {
+  return (
+    <>
+      <h5>cart</h5>
+    </>
+  );
+}
+
+const mapStateToProps = (store) => {
+  return {};
+};
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, {})(Test);
