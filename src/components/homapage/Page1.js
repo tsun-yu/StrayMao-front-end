20c9d7dc747b8871601a5db5a443db1ec347f7ba
Wayne <tsun0227@yahoo.com.tw>
2020-10-27 17:25:33 +0800

    homepage page-1(tmp)

--- src/components/homapage/Page1.js
+++ src/components/homapage/Page1.js
@@ -3,19 +3,21 @@ import React, { useState, useEffect } from 'react'
 function Page1(props) {
 return(
 <>
-<div className="row container">
-    <div className="col-5">
-    <div className="page1des">
-    Love them likes <br/>
-    family.
+<div className="d-flex">
+    <div>
+        <div className="page1des">
+        Love them likes <br/>
+        family.
+        </div>
+        <div className="page1des2 d-flex align-items-center">
+            <div className='mx-auto'>
+            給他們一個機會, 擁有一個家。
+            </div>
+        </div>
     </div>
-    <div className="page1des2 d-flex align-items-center">
-    <div className='mx-auto'>
-    給他們一個機會, 擁有一個家。
+    <div className="imgDiv">
+        <img src="/image/homepage/homepage-1.jpg"/>
     </div>
-    </div>
-    </div>
-    <div className="row col-7 img"></div>
 </div>
 </>
 )}
