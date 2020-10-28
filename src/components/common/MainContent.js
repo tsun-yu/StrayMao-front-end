38f1f32194df67b259f8a0b69a77ea4939036cd3
joart0725 <joart0725@gmail.com>
2020-10-28 14:53:32 +0800

    改命名為小駝峰, 增加共用英文字型

--- src/components/common/MainContent.js
+++ src/components/common/MainContent.js
@@ -8,16 +8,23 @@ function MainContent(props) {
         {/* props.children代表嵌入在其中的元件或資料內容 */}
         <div className="container">
           {props.children}
-          <div className="Header1">標題一請自取</div>
-          <div className="Header2">標題二請自取</div>
-          <div className="Header3">標題三請自取</div>
-          <div className="Header4">標題四請自取</div>
-          <div className="Header5">標題五請自取</div>
-          <div className="Header6">標題六請自取</div>
-          <div className="Header7">標題七請自取</div>
-          <div className="Paragraph1">內文一請自取</div>
-          <div className="Paragraph2">內文二請自取</div>
+          <div className="header1">標題一請自取</div>
+          <div className="header2">標題二請自取</div>
+          <div className="header3">標題三請自取</div>
+          <div className="header4">標題四請自取</div>
+          <div className="header5">標題五請自取</div>
+          <div className="header6">標題六請自取</div>
+          <div className="header7">標題七請自取</div>
+          <div className="paragraph1">內文一請自取</div>
+          <div className="paragraph2">內文二請自取</div>
           <div className="caption">說明文字請自取</div>
+          <div className="enHeader1">StrayMao Here</div>
+          <div className="enHeader2">StrayMao Here</div>
+          <div className="enHeader3">StrayMao Here</div>
+          <div className="enHeader4">StrayMao Here</div>
+          <div className="enHeader5">StrayMao Here</div>
+          <div className="enHeader6">StrayMao Here</div>
+          <div className="enParagraph">StrayMao Here</div>
         </div>
       </main>
     </>
