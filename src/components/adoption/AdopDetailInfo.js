2fd772a09eff2bcd0dc286a0b2d29752754f6153
krok1029 <krok1029@gmail.com>
2020-10-29 19:57:56 +0800

    adop list  ver0.5

--- src/components/adoption/AdopDetailInfo.js
+++ src/components/adoption/AdopDetailInfo.js
@@ -7,65 +7,69 @@ import AdopMainReadMore from "./AdopMainReadMore";
 function AdopDetailInfo(props) {
   return (
     <>
-      <div className="adopDetailInfo">
-        <ul className="adoptListStyleNone">
-          <li>
-            <span className="adopDetailQues">救援經過：</span>
-            <span className="adopDetailAns">
+      <div className="adopDetailInfo p-5">
+        <ul className="adoptListStyleNone paragraph2 w-100">
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">救援經過：</span>
+            <div className="adopDetailAns">
               被人用老鼠籠誘捕要丟到山區野放。
-            </span>
+            </div>
           </li>
-          <li>
-            <span className="adopDetailQues">預防針施打狀況：</span>
-            <span className="adopDetailAns">每年都有打3合一疫苗</span>
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">預防針施打狀況：</span>
+            <div className="adopDetailAns ">每年都有打3合一疫苗</div>
           </li>
-          <li>
-            <span className="adopDetailQues">
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">
               血檢快篩貓瘟、貓白血、貓愛滋是否通過：
             </span>
             <span className="adopDetailAns">均過關</span>
           </li>
-          <li>
-            <span className="adopDetailQues">上一次體內驅蟲時間：</span>
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">
+              上一次體內驅蟲時間：
+            </span>
             <span className="adopDetailAns">108/12</span>
           </li>
-          <li>
-            <span className="adopDetailQues">上一次體外驅蟲時間：</span>
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">
+              上一次體外驅蟲時間：
+            </span>
             <span className="adopDetailAns">109/1</span>
           </li>
-          <li>
-            <span className="adopDetailQues">是否結紮：</span>
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">是否結紮：</span>
             <span className="adopDetailAns">是</span>
           </li>
-          <li>
-            <span className="adopDetailQues">是否已植入晶片：</span>
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">是否已植入晶片：</span>
             <span className="adopDetailAns">是</span>
           </li>
-          <li>
-            <span className="adopDetailQues">是否親貓：</span>
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues paragraph2">是否親貓：</span>
             <span className="adopDetailAns">親</span>
           </li>
-          <li>
-            <span className="adopDetailQues">是否親人：</span>
+          <li className="d-flex flex-row justify-content-between">
+            <span className="adopDetailQues ">是否親人：</span>
             <span className="adopDetailAns">親</span>
           </li>
-          <li>
+          <li className="d-flex flex-row justify-content-between">
             <span className="adopDetailQues">是否親狗：</span>
             <span className="adopDetailAns">不知道</span>
           </li>
-          <li>
+          <li className="d-flex flex-row justify-content-between">
             <span className="adopDetailQues">有沒有特別習慣：</span>
             <span className="adopDetailAns">
               有時候拍屁屁她會生氣，有時候很爽
             </span>
           </li>
-          <li>
+          <li className="d-flex flex-row justify-content-between">
             <span className="adopDetailQues">
               有沒有什麼需要特別照顧的地方：
             </span>
             <span className="adopDetailAns">無</span>
           </li>
-          <li>
+          <li className="d-flex flex-row justify-content-between">
             <span className="adopDetailQues">其他交代事項：</span>
             <span className="adopDetailAns">留意皮膚狀況</span>
           </li>
