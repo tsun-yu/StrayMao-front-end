f7dfa7b4f9df3bb4461e6403322e77397fd6b49e
Wayne <tsun0227@yahoo.com.tw>
2020-10-31 16:32:39 +0800

    page2 change pic

--- src/components/homapage/Homepage.js
+++ src/components/homapage/Homepage.js
@@ -6,18 +6,17 @@ import Page4 from './Page4'
 import Page5 from './Page5'
 import Page6 from './Page6'
 
-
 function homepage(props) {
-return(
-<>
-<Page1 />
-<Page2 />
-<Page3 />
-<Page4 />
-<Page5 />
-<Page6 />
-
-</>
-)}
+  return (
+    <>
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
+      <Page5 />
+      <Page6 />
+    </>
+  )
+}
 
 export default homepage
