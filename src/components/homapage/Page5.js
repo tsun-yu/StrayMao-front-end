987229412facb0b0852916cac796470e3f5e7a49
Wayne <tsun0227@yahoo.com.tw>
2020-11-02 11:44:02 +0800

    page5card done

--- src/components/homapage/Page5.js
+++ src/components/homapage/Page5.js
@@ -31,59 +31,21 @@ function Page5(props) {
                     <div className="page5cardImage mx-auto">
                       background image
                     </div>
-                    <div className="page5cardDes mx-auto">
-                      <div className="page5cardDate">
-                        <div className="page5cardYear">2020</div>
-                        <div className="page5cardDay">10.10</div>
+                    <div className="page5cardDes mx-auto d-flex">
+                      <div className="page5cardDate my-auto d-flex flex-column justify-content-center">
+                        <div className="page5cardYear">2020.</div>
+                        <div className="page5cardDay text-right">10.10</div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="page5card">
-                    <div className="page5cardImage mx-auto">
-                      background image
-                    </div>
-                    <div className="page5cardDes mx-auto">
-                      <div className="page5cardDate">
-                        <div className="page5cardYear">2020</div>
-                        <div className="page5cardDay">10.10</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="page5card">
-                    <div className="page5cardImage mx-auto">
-                      background image
-                    </div>
-                    <div className="page5cardDes mx-auto">
-                      <div className="page5cardDate">
-                        <div className="page5cardYear">2020</div>
-                        <div className="page5cardDay">10.10</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="page5card">
-                    <div className="page5cardImage mx-auto">
-                      background image
-                    </div>
-                    <div className="page5cardDes mx-auto">
-                      <div className="page5cardDate">
-                        <div className="page5cardYear">2020</div>
-                        <div className="page5cardDay">10.10</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="page5card">
-                    <div className="page5cardImage mx-auto">
-                      background image
-                    </div>
-                    <div className="page5cardDes mx-auto">
-                      <div className="page5cardDate">
-                        <div className="page5cardYear">2020</div>
-                        <div className="page5cardDay">10.10</div>
+                      <div className="page5cardContent my-auto d-flex flex-column justify-content-between">
+                        <div className="page5cardLabel text-center">日常</div>
+                        <div className="page5cardTopic text-right">
+                          發現我家毛小孩...
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="page5right-2">sdf</div>
+                <div className="page5right-2"></div>
               </div>
             </div>
           </div>
