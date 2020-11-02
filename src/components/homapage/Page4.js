3c2e6c397e248c38855fb29f6d4c0fbab05c439c
Wayne <tsun0227@yahoo.com.tw>
2020-11-02 20:12:36 +0800

    page4 component

--- src/components/homapage/Page4.js
+++ src/components/homapage/Page4.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Page4Card from './Page4Card'
 
 function Page4(props) {
   return (
@@ -11,8 +12,10 @@ function Page4(props) {
       </div>
       <div className="container page4">
         <div className="row">
-          <div className="col-lg-3 page4product">
-            <div className="page4label">狗 狗 用</div>
+          {/* <div className="col-lg-3 page4product">
+            <div className="page4label">
+              <div className="p-1">狗 狗 用</div>
+            </div>
             <div className="page4img">
               <img src="./image/homepage/homepage4-1.png"></img>
             </div>
@@ -26,55 +29,11 @@ function Page4(props) {
                 <div className="page4item text-right">最方便自動餵食器</div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3 page4product mt-5">
-            <div className="page4label">狗 狗 用</div>
-            <div className="page4img">
-              <img src="./image/homepage/homepage4-1.png"></img>
-            </div>
-            <div className="page4text px-3 py-2 d-flex">
-              <div className="page4date pr-1">
-                <div className="page4year">2020.</div>
-                <div className="page4new text-right">New</div>
-              </div>
-              <div className="page4content d-flex flex-column justify-content-end">
-                <div className="page4popular">本月品牌熱門</div>
-                <div className="page4item text-right">最方便自動餵食器</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-3 page4product">
-            <div className="page4label">狗 狗 用</div>
-            <div className="page4img">
-              <img src="./image/homepage/homepage4-1.png"></img>
-            </div>
-            <div className="page4text px-3 py-2 d-flex">
-              <div className="page4date pr-1">
-                <div className="page4year">2020.</div>
-                <div className="page4new text-right">New</div>
-              </div>
-              <div className="page4content d-flex flex-column justify-content-end">
-                <div className="page4popular">本月品牌熱門</div>
-                <div className="page4item text-right">最方便自動餵食器</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-3 page4product mt-5">
-            <div className="page4label">狗 狗 用</div>
-            <div className="page4img">
-              <img src="./image/homepage/homepage4-1.png"></img>
-            </div>
-            <div className="page4text px-3 py-2 d-flex">
-              <div className="page4date pr-1">
-                <div className="page4year">2020.</div>
-                <div className="page4new text-right">New</div>
-              </div>
-              <div className="page4content d-flex flex-column justify-content-end">
-                <div className="page4popular">本月品牌熱門</div>
-                <div className="page4item text-right">最方便自動餵食器</div>
-              </div>
-            </div>
-          </div>
+          </div> */}
+          <Page4Card />
+          <Page4Card />
+          <Page4Card />
+          <Page4Card />
         </div>
       </div>
     </>
---
+++ src/components/homapage/Page4Card.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+
+function Page4Card(props) {
+  return (
+    <>
+      <div className="col-lg-3 page4product">
+        <div className="page4label">
+          <div className="p-1">狗 狗 用</div>
+        </div>
+        <div className="page4img">
+          <img src="./image/homepage/homepage4-1.png"></img>
+        </div>
+        <div className="page4text px-3 py-2 d-flex">
+          <div className="page4date pr-1">
+            <div className="page4year">2020.</div>
+            <div className="page4new text-right">New</div>
+          </div>
+          <div className="page4content d-flex flex-column justify-content-end">
+            <div className="page4popular">本月品牌熱門</div>
+            <div className="page4item text-right">最方便自動餵食器</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Page4Card
