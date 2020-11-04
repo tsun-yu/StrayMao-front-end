6d1e0a5d5af720eeed92a99c83f72977c1c200ce
jiang490094 <jfr08100810@gmail.com>
2020-11-04 15:30:02 +0800

    completed animation for bounce mouse

--- src/components/store/StoreCard.js
+++ src/components/store/StoreCard.js
@@ -5,7 +5,9 @@ import { connect } from 'react-redux'
 function StoreCard() {
   return (
     <>
-      <div className="card_new col-lg-3">
+      {/* <div className="container">
+        <div className="row"> */}
+      <div className="card_new col-lg-3 col-md-6 col-sm-6">
         <div className="icon d-flex">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -47,9 +49,9 @@ function StoreCard() {
           <div className="icon2 d-flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="26.865"
-              height="27.721"
-              viewBox="0 0 26.865 27.721"
+              width="20.793"
+              height="21.442"
+              viewBox="0 0 20.793 21.442"
             >
               <g
                 id="Group_607"
@@ -62,7 +64,7 @@ function StoreCard() {
                   cx="1.98"
                   cy="1.98"
                   r="1.98"
-                  transform="translate(7.983 22.76)"
+                  transform="translate(5.768 16.481)"
                   fill="#fffffd"
                   stroke="#ed8f8f"
                   stroke-width="1"
@@ -71,7 +73,7 @@ function StoreCard() {
                   id="Path_1085"
                   data-name="Path 1085"
                   d="M268.477,324.1h0a1.98,1.98,0,1,0,1.98,1.98A1.98,1.98,0,0,0,268.477,324.1Z"
-                  transform="translate(-246.608 -301.338)"
+                  transform="translate(-252.682 -307.617)"
                   fill="#fffffd"
                   stroke="#ed8f8f"
                   stroke-width="1"
@@ -79,7 +81,7 @@ function StoreCard() {
                 <path
                   id="Path_1086"
                   data-name="Path 1086"
-                  d="M31.8,19.73H15.326L15,17.583H28.541a.978.978,0,0,0,.9-.7l3.223-9.328a1.17,1.17,0,0,0-.545-1.44.857.857,0,0,0-.357-.077H13.327L12.554.925A1.014,1.014,0,0,0,11.587,0H7.88A1.046,1.046,0,0,0,6.913,1.11,1.046,1.046,0,0,0,7.88,2.221h2.9l2.772,18.8a1.014,1.014,0,0,0,.967.925H31.8a1.121,1.121,0,0,0,0-2.221Z"
+                  d="M25.949,14.086h-12.6L13.1,12.554H23.459a.742.742,0,0,0,.69-.5l2.466-6.66A.812.812,0,0,0,26.2,4.363a.7.7,0,0,0-.273-.055H11.82L11.228.66A.756.756,0,0,0,10.488,0H7.653a.768.768,0,0,0-.74.793.768.768,0,0,0,.74.793H9.872l2.121,13.426a.756.756,0,0,0,.74.661H25.949a.795.795,0,0,0,0-1.586Z"
                   transform="translate(-6.913 0.001)"
                   fill="#fffffd"
                   stroke="#ed8f8f"
@@ -90,6 +92,8 @@ function StoreCard() {
           </div>
         </div>
       </div>
+      {/* </div>
+      </div> */}
     </>
   )
 }
