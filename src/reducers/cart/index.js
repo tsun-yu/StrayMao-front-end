6f58ac9e0f4aa83591c60a940f8538b4aea61913
annelin27 <69293236+annelin27@users.noreply.github.com>
2020-11-06 12:02:05 +0800

    1106

--- src/reducers/cart/index.js
+++ src/reducers/cart/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 
-import {} from "./main";
+import { getRecom, goodsHeart } from "./main";
 
 // //combine all reducer to a big reducer
-export const cartReducer = combineReducers({});
+export const cartReducer = combineReducers({
+    getRecom,
+    goodsHeart,
+});
