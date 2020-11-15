c36fa070ce6c50427f36a1d90ebe4d6e60639ec4
Wayne <tsun0227@yahoo.com.tw>
2020-11-15 22:18:47 +0800

    navbar

--- src/components/homapage/Donate.js
+++ src/components/homapage/Donate.js
@@ -7,63 +7,131 @@ function Donate(props) {
         <div className="donateBanner mx-auto mt-5 d-flex justify-content-center align-items-center">
           DONATE
         </div>
-        <div className="donateTitle d-flex justify-content-center my-5 mr-3">
-          募款專案
+        <div className="donateTitle d-flex justify-content-center my-5 mr-3 align-items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="492"
+            height="3"
+            viewBox="0 0 492 3"
+          >
+            <line
+              id="Line_32"
+              data-name="Line 32"
+              x1="492"
+              transform="translate(0 1.5)"
+              fill="none"
+              stroke="#cb997e"
+              stroke-width="3"
+            />
+          </svg>
+          <div className="mx-3">募款專案</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="492"
+            height="3"
+            viewBox="0 0 492 3"
+          >
+            <line
+              id="Line_32"
+              data-name="Line 32"
+              x1="492"
+              transform="translate(0 1.5)"
+              fill="none"
+              stroke="#cb997e"
+              stroke-width="3"
+            />
+          </svg>
         </div>
         <div className="donateArea">
           <div className="container">
+            <div
+              className="row justify-content-center my-3
+            "
+            >
+              {/* 單次 */}
+              <div className="mr-2">
+                <input
+                  className="magic-radio justify-content-center align-items-center"
+                  type="radio"
+                  name="radio"
+                  id="r1"
+                  value="option1"
+                />
+                <label htmlFor="r1" className="paragraph1 mr-1">
+                  單次
+                </label>
+              </div>
+              {/* 每月一次 */}
+              <div className="ml-2">
+                <input
+                  className="magic-radio"
+                  type="radio"
+                  name="radio"
+                  id="r2"
+                  value="option2"
+                />
+                <label htmlFor="r2" className="paragraph1">
+                  每月一次
+                </label>
+              </div>
+            </div>
             {/* buttom */}
-            <div className="row justify-content-center">
-              <button
-                className="btn-green mx-2"
-                type="button"
-                style={{ width: '100px', padding: '10px' }}
-                onClick={() => {
-                  document.querySelector('.donateAmount').value = '300'
-                }}
-              >
-                NT$ 300
-              </button>
-              <button
-                className="btn-green  mx-2"
-                type="button"
-                style={{ width: '100px', padding: '10px' }}
-                onClick={() => {
-                  document.querySelector('.donateAmount').value = '500'
-                }}
-              >
-                NT$ 500
-              </button>
-              <button
-                className="btn-green mx-2"
-                type="button"
-                style={{ width: '100px', padding: '10px' }}
-                onClick={() => {
-                  document.querySelector('.donateAmount').value = '1000'
-                }}
-              >
-                NT$ 1000
-              </button>{' '}
-              <button
-                className="btn-green mx-2"
-                type="button"
-                style={{ width: '100px', padding: '10px' }}
-                onClick={() => {
-                  document.querySelector('.donateAmount').value = '3000'
-                }}
-              >
-                NT$ 3000
-              </button>{' '}
-              <button
-                className="btn-green mx-2"
-                type="button"
-                style={{ width: '100px', padding: '10px' }}
-                onClick={() => {
-                  document.querySelector('.donateAmount').value = '5000'
-                }}
-              >
-                NT$ 5000
-              </button>
+            <div
+              className="row justify-content-center flex-column
+            "
+            >
+              <div className="d-flex justify-content-center">
+                <button
+                  className="btn-green mx-2"
+                  type="button"
+                  style={{ width: '100px', padding: '10px' }}
+                  onClick={() => {
+                    document.querySelector('.donateAmount').value = '300'
+                  }}
+                >
+                  NT$ 300
+                </button>
+                <button
+                  className="btn-green  mx-2"
+                  type="button"
+                  style={{ width: '100px', padding: '10px' }}
+                  onClick={() => {
+                    document.querySelector('.donateAmount').value = '500'
+                  }}
+                >
+                  NT$ 500
+                </button>
+                <button
+                  className="btn-green mx-2"
+                  type="button"
+                  style={{ width: '100px', padding: '10px' }}
+                  onClick={() => {
+                    document.querySelector('.donateAmount').value = '1000'
+                  }}
+                >
+                  NT$ 1000
+                </button>{' '}
+                <button
+                  className="btn-green mx-2"
+                  type="button"
+                  style={{ width: '100px', padding: '10px' }}
+                  onClick={() => {
+                    document.querySelector('.donateAmount').value = '3000'
+                  }}
+                >
+                  NT$ 3000
+                </button>{' '}
+                <button
+                  className="btn-green mx-2"
+                  type="button"
+                  style={{ width: '100px', padding: '10px' }}
+                  onClick={() => {
+                    document.querySelector('.donateAmount').value = '5000'
+                  }}
+                >
+                  NT$ 5000
+                </button>
+              </div>
             </div>
             {/* 信用卡輸入 */}
             <div className="mt-5 row justify-content-center">
@@ -79,16 +147,16 @@ function Donate(props) {
                 className="d-flex flex-column align-items-start ml-3
             "
               >
+                {/* 金額 */}
                 <div>
-                  {/* 金額 */}
                   <input
                     className="input-brown donateAmount pl-3"
                     type="text"
                     style={{ width: '265px' }}
                   ></input>
                 </div>
+                {/* 卡號 */}
                 <div className="mt-4">
-                  {/* 卡號 */}
                   <input
                     className="input-brown cdcard-1"
                     type="text"
@@ -110,16 +178,16 @@ function Donate(props) {
                     style={{ width: '55px' }}
                   ></input>
                 </div>
+                {/* 後三碼 */}
                 <div className="mt-2">
-                  {/* 後三碼 */}
                   <input
                     className="input-brown cdcardthree"
                     type="password"
                     style={{ width: '55px' }}
                   ></input>
                 </div>
+                {/* 到期日 */}
                 <div className="mt-2">
-                  {/* 到期日 */}
                   <input
                     className="input-brown cdcardmon"
                     type="text"
