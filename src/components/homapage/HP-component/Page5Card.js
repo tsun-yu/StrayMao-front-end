9e1d30bd417f46653f5cbc7dd6b447fb541a2585
Wayne <tsun0227@yahoo.com.tw>
2020-11-18 22:33:50 +0800

    content

--- src/components/homapage/HP-component/Page5Card.js
+++ src/components/homapage/HP-component/Page5Card.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react'
 
 function Page5Card(props) {
+  const { page5cardClass, img, date, title } = props
   return (
     <>
-      <div className={props.page5cardClass}>
+      <div className={page5cardClass}>
         <div className=" mx-auto page5cardImg">
-          <img className="" src={props.img} />
+          <img className="" src={img} />
         </div>
         <div className="page5cardDes mx-auto d-flex">
           <div className="page5cardDate my-auto d-flex flex-column justify-content-center">
             <div className="page5cardYear">2020.</div>
-            <div className="page5cardDay text-right">10.10</div>
+            <div className="page5cardDay text-right">{date}</div>
           </div>
           <div className="page5cardContent my-auto d-flex flex-column justify-content-between">
             <div className="page5cardLabel text-center">日常</div>
-            <div className="page5cardTopic text-right">發現我家毛小孩...</div>
+            <div className="page5cardTopic text-right">{title}</div>
           </div>
         </div>
       </div>
